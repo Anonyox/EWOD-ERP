5d13a96a104d21a50762d65723e2f685b3376703
Pedro <79409385+PedroHenrique025@users.noreply.github.com>
2021-05-02 20:14:05 -0300

    kfhdiogfi

--- PROJETO TCC/TCC/VISÃO/EditarUsuario.cs
+++ PROJETO TCC/TCC/VISÃO/EditarUsuario.cs
@@ -15,7 +15,8 @@ namespace TCC.VISÃO
 {
     public partial class EditarUsuario : Form
     {
-        SqlConnection con = new SqlConnection(@"Data Source= tcp: 177.125.224.84,9022;Initial Catalog=tcc;User ID=etec;Password=123456;connection timeout = 1");
+        Conexao con = new Conexao();
+        //SqlConnection con = new SqlConnection(@"Data Source= tcp: 26.186.226.18,9022;Initial Catalog=tcc;User ID=etec;Password=123456;connection timeout = 1");
         SqlDataReader dr;
 
         int validamsg = 0;
@@ -48,9 +49,9 @@ namespace TCC.VISÃO
         {
             // TODO: esta linha de código carrega dados na tabela 'tccDataSet.logins'. Você pode movê-la ou removê-la conforme necessário.
             this.loginsTableAdapter.Fill(this.tccDataSet.logins);
-            con.Open();
+            con.conectar();
 
-            SqlCommand cmd = new SqlCommand("SELECT usuario FROM logins", con);
+            SqlCommand cmd = new SqlCommand("SELECT usuario FROM logins", con.conectar());
             //cmd.CommandText = ;
 
             dr = cmd.ExecuteReader();
@@ -68,7 +69,7 @@ namespace TCC.VISÃO
             txtnomeUsuario.AutoCompleteCustomSource = collection;
 
             dr.Close();
-            con.Close();
+            con.desconectar();
 
 
         }
@@ -114,9 +115,9 @@ namespace TCC.VISÃO
         public void txtnomeUsuario_Leave_1(object sender, EventArgs e)
         {
             string usuario = Convert.ToString(txtnomeUsuario.Text);
-            con.Open();
+            con.conectar();
             //Elaborar Select que contenha cada um dos campos da tabela
-            SqlCommand cmd = new SqlCommand("SELECT cidade,endereco,complemento,bairro,telefone,CEP,estado,perfil,departamento FROM logins WHERE usuario LIKE @param ", con);
+            SqlCommand cmd = new SqlCommand("SELECT cidade,endereco,complemento,bairro,telefone,CEP,estado,perfil,departamento FROM logins WHERE usuario LIKE @param ", con.conectar());
             cmd.Parameters.AddWithValue("@param", txtnomeUsuario.Text + "%");
             dr = cmd.ExecuteReader();
             //criar variáveis para armazenar os campos
@@ -149,39 +150,39 @@ namespace TCC.VISÃO
 
 
             dr.Close();
-            con.Close();
+            con.desconectar();
 
         }
 
         private void btnConfirmar_Click(object sender, EventArgs e)
-        {            
+        {
             string SenhaAdm = "";
-            con.Open();
-            SqlCommand cmd = new SqlCommand("SELECT senha FROM logins WHERE perfil = 'Administrador'",con);
+            con.conectar();
+            SqlCommand cmd = new SqlCommand("SELECT senha FROM logins WHERE perfil = 'Administrador'", con.conectar());
             cmd.Parameters.AddWithValue("senha", SenhaAdm);
             dr = cmd.ExecuteReader();
 
             dr.Close();
-            con.Close();
+            con.desconectar();
 
-            if(txtnomeUsuario.Text == "")
+            if (txtnomeUsuario.Text == "")
             {
                 MessageBox.Show("Defina o Usuário que deseja Alterar", "Erro de Confirmação!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                 
-                if(txtsenhaAdm.Text == SenhaAdm)
+
+                if (txtsenhaAdm.Text == SenhaAdm)
                 {
-                    con.Open();
-                    SqlCommand cmd2 = new SqlCommand("UPDATE SET cidade = @cidade, endereco = @endereco,complemento = @complemento,bairro = @bairro,telefone = @telefone, CEP = @CEP,estado = @estado,perfil = @perfil,departamento = @departemento WHERE usuario = @usuario");
-                        cmd2.Parameters.AddWithValue("@cidade",txtCidade.Text);
-                        cmd2.Parameters.AddWithValue("@usuario", txtnomeUsuario.Text);
-                        cmd2.Parameters.AddWithValue("@endereco", txtenderecoUsuario.Text);
-                        cmd2.Parameters.AddWithValue("@complemento", txtcomplementoUsuario.Text);
-                        cmd2.Parameters.AddWithValue("@bairro", txtbairroUsuario.Text);
-                        cmd2.Parameters.AddWithValue("@telefone", txttelefoneUsuario.Text);
-                        cmd2.Parameters.AddWithValue("@CEP", txtcepUsuario.Text);
-                        cmd2.Parameters.AddWithValue("@estado", cbestadoUsuario.Text);
-                        cmd2.Parameters.AddWithValue("@perfil", cbperfilUsuario.Text);
-                        cmd2.Parameters.AddWithValue("@departamento", cbdepartamentoUsuario.Text);
+                    con.conectar();
+                    SqlCommand cmd2 = new SqlCommand("UPDATE SET cidade = @cidade, endereco = @endereco,complemento = @complemento,bairro = @bairro,telefone = @telefone, CEP = @CEP,estado = @estado,perfil = @perfil,departamento = @departemento WHERE usuario = @usuario", con.conectar());
+                    cmd2.Parameters.AddWithValue("@cidade", txtCidade.Text);
+                    cmd2.Parameters.AddWithValue("@usuario", txtnomeUsuario.Text);
+                    cmd2.Parameters.AddWithValue("@endereco", txtenderecoUsuario.Text);
+                    cmd2.Parameters.AddWithValue("@complemento", txtcomplementoUsuario.Text);
+                    cmd2.Parameters.AddWithValue("@bairro", txtbairroUsuario.Text);
+                    cmd2.Parameters.AddWithValue("@telefone", txttelefoneUsuario.Text);
+                    cmd2.Parameters.AddWithValue("@CEP", txtcepUsuario.Text);
+                    cmd2.Parameters.AddWithValue("@estado", cbestadoUsuario.Text);
+                    cmd2.Parameters.AddWithValue("@perfil", cbperfilUsuario.Text);
+                    cmd2.Parameters.AddWithValue("@departamento", cbdepartamentoUsuario.Text);
                     dr = cmd2.ExecuteReader();
 
                     MessageBox.Show("Alteração realizada com Sucesso!!", "Ação realizada", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -199,7 +200,7 @@ namespace TCC.VISÃO
 
 
                     dr.Close();
-                    con.Close();
+                    con.desconectar();
 
                 }
                 else if (txtsenhaAdm.Text != SenhaAdm)
