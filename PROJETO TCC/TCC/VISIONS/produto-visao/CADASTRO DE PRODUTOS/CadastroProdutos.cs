4a3be587585644157a0149126e1673f054d21f87
Pedro <79409385+PedroHenrique025@users.noreply.github.com>
2021-05-20 20:54:57 -0300

    ORGANIZAÇÃO PADRÃO

--- PROJETO TCC/TCC/VISIONS/produto-visao/CADASTRO DE PRODUTOS/CadastroProdutos.cs
+++ PROJETO TCC/TCC/VISIONS/produto-visao/CADASTRO DE PRODUTOS/CadastroProdutos.cs
@@ -9,13 +9,9 @@ namespace TCC.VISÃO
 {
     public partial class CadastroProduto : Form
     {
-        menuAdministrador menuz = new menuAdministrador();
-        cadastroprodutoDaoComandos cadpro = new cadastroprodutoDaoComandos();
-        
-        SqlDataReader dr;
-        Conexao con = new Conexao();
-
        
+
+        #region CONTROLADOR 
         public CadastroProduto()
         {
             InitializeComponent();
@@ -25,17 +21,17 @@ namespace TCC.VISÃO
 
 
         }
+        #endregion
 
-        private void btnSair_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
+        #region VARIÁVEIS E INTÂNCIAS
+        menuAdministrador menuz = new menuAdministrador();
+        cadastroprodutoDaoComandos cadpro = new cadastroprodutoDaoComandos();
 
-        private void CADASTRARPRODUTO_FormClosed(object sender, FormClosedEventArgs e)
-        {
-            //menuz.Show();
-        }
+        SqlDataReader dr;
+        Conexao con = new Conexao();
+        #endregion
 
+        #region MÉTODOS DE FUNCIONALIDADES
         private void listarProdutos()
         {
             Conexao con = new Conexao();
@@ -68,51 +64,10 @@ namespace TCC.VISÃO
 
         }
 
-        private void btnCadastrar_MouseEnter(object sender, EventArgs e)
-        {
-            //btnCadastrar.Size = new Size(100, 50);
-        }
-
-        private void btnCadastrar_MouseLeave(object sender, EventArgs e)
-        {
-           // btnCadastrar.Size = new Size(79, 35);
-        }
-
-       
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            listarProdutos();
-            timer1.Start();
-
-            
-        }
-
-        private void CadastroProduto_Load(object sender, EventArgs e)
-        {           
-            buscarProduto();                  
-            timer1.Start();           
-        }
-
-        private void textBox6_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnConfirmar_MouseEnter(object sender, EventArgs e)
-        {
-            lblconfirmar.Visible = true;
-        }
-
-        private void btnConfirmar_MouseLeave(object sender, EventArgs e)
-        {
-            lblconfirmar.Visible = false;
-        }
-
         public void buscarProduto()
         {
 
-            SqlCommand cmd = new SqlCommand("SELECT nome FROM produtos", con.conectar());          
+            SqlCommand cmd = new SqlCommand("SELECT nome FROM produtos", con.conectar());
 
             dr = cmd.ExecuteReader();
 
@@ -138,13 +93,13 @@ namespace TCC.VISÃO
         {
             SqlCommand command = new SqlCommand("SELECT quantidade, valordeCompra, valordeVenda FROM produtos WHERE nome = @nome", con.conectar());
             command.Parameters.AddWithValue("@nome", txtnomeProduto.Text);
-            
+
             dr = command.ExecuteReader();
 
             while (dr.Read())
             {
                 //txtcodigoProduto.Text = dr["codProduto"].ToString();
-                txtquantidadeProduto.Text = dr["quantidade"].ToString();              
+                txtquantidadeProduto.Text = dr["quantidade"].ToString();
                 txtvalorCompra.Text = dr["valordeCompra"].ToString();
                 txtvalorVenda.Text = dr["valordeVenda"].ToString();
                 //cmbtipo.Text = dr["tipo"].ToString();
@@ -155,9 +110,71 @@ namespace TCC.VISÃO
             con.desconectar();
         }
 
+        #endregion
+
+        #region DESIGN
+        private void btnCadastrar_MouseEnter(object sender, EventArgs e)
+        {
+            //btnCadastrar.Size = new Size(100, 50);
+        }
+
+        private void btnCadastrar_MouseLeave(object sender, EventArgs e)
+        {
+            // btnCadastrar.Size = new Size(79, 35);
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            listarProdutos();
+            timer1.Start();
+
+
+        }
+
+        private void CadastroProduto_Load(object sender, EventArgs e)
+        {
+            buscarProduto();
+            timer1.Start();
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConfirmar_MouseEnter(object sender, EventArgs e)
+        {
+            lblconfirmar.Visible = true;
+        }
+
+        private void btnConfirmar_MouseLeave(object sender, EventArgs e)
+        {
+            lblconfirmar.Visible = false;
+
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void CADASTRARPRODUTO_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //menuz.Show();
+        }
+
         private void txtnomeProduto_Leave(object sender, EventArgs e)
         {
             preencherCampos();
         }
+
+        #endregion
+
+
+       
+
+        
     }
 }
